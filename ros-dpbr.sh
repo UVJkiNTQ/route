96e92621cb167b44698cb328ee1ce8ddb4e6eae3
UVJkiNTQ <12570945+UVJkiNTQ@users.noreply.github.com>
2025-04-16 21:41:57 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -2,17 +2,29 @@
 mkdir -p ./pbr
 cd ./pbr
 
-# AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/refs/heads/release/text/cn.txt
 
 {
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=CN address=$net comment=CN-IP"
 done
 
 } > ../CN.rsc
+
+
+
+wget --no-check-certificate -c -O JP.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/refs/heads/release/text/jp.txt
+
+{
+echo "/ip firewall address-list"
+
+for net in $(cat JP.txt) ; do
+  echo "add list=JP address=$net comment=JP-IP"
+done
+
+} > ../JP.rsc
 
 cd ..
 rm -rf ./pbr
