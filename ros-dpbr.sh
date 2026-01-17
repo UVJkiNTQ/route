8838a02d6c682fea9eaa1c4645309249e714e778
UVJkiNTQ <12570945+UVJkiNTQ@users.noreply.github.com>
2026-01-18 00:29:32 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -7,13 +7,10 @@ wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/Loyal
 {
 echo "/ip firewall address-list"
 
-# 使用while循环逐行读取文件
 while IFS= read -r net || [ -n "$net" ]; do
-  # 跳过空行和注释行
   [ -z "$net" ] && continue
   [[ "$net" == \#* ]] && continue
   
-  # 检查是否是IPv4地址（包含.且不包含:）
   if [[ "$net" == *.* && "$net" != *:* ]]; then
     echo "add list=CN address=$net comment=CN-IP"
   fi
@@ -28,13 +25,10 @@ wget --no-check-certificate -c -O JP.txt https://raw.githubusercontent.com/Loyal
 {
 echo "/ip firewall address-list"
 
-# 使用while循环逐行读取文件
 while IFS= read -r net || [ -n "$net" ]; do
-  # 跳过空行和注释行
   [ -z "$net" ] && continue
   [[ "$net" == \#* ]] && continue
   
-  # 检查是否是IPv4地址（包含.且不包含:）
   if [[ "$net" == *.* && "$net" != *:* ]]; then
     echo "add list=JP address=$net comment=JP-IP"
   fi
