65226261a336ae7e5f6db7a1875a760d96d99f96
UVJkiNTQ <12570945+UVJkiNTQ@users.noreply.github.com>
2026-01-18 00:19:49 +0800

    Fix case sensitivity in country file references

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -7,7 +7,7 @@ wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/Loyal
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
+for net in $(cat cn.txt) ; do
   # 检查是否是IPv4地址（包含.且不包含:）
   if [[ $net == *.* && $net != *:* ]]; then
     echo "add list=CN address=$net comment=CN-IP"
@@ -23,7 +23,7 @@ wget --no-check-certificate -c -O JP.txt https://raw.githubusercontent.com/Loyal
 {
 echo "/ip firewall address-list"
 
-for net in $(cat JP.txt) ; do
+for net in $(cat jp.txt) ; do
   # 检查是否是IPv4地址（包含.且不包含:）
   if [[ $net == *.* && $net != *:* ]]; then
     echo "add list=JP address=$net comment=JP-IP"
